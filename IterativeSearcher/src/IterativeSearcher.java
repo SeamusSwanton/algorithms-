7446d28e44cf5b0d355b120dce816f56e1037111
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-09-27 14:15:33 -0600

    Iterative Searcher initial commit

---
+++ IterativeSearcher/src/IterativeSearcher.java
@@ -0,0 +1,37 @@
+
+public class IterativeSearcher {
+
+	public int doBinarySearch(IntegerArray arrayToSort, int i) {
+		
+		int index = -1;
+		int start = 0;
+		int end = arrayToSort.length() - 1;
+		int mid = (end - start)/2;
+		
+		while ((end - start) > 1)
+		{
+			if (arrayToSort.read(mid) < i)
+			{
+				start = mid;
+				mid = mid + (end - start)/2;
+			}
+			else
+			{
+				end = mid;
+				mid = mid - (end - start)/2;
+			}
+		}
+		
+		if (arrayToSort.read(start) == i)
+		{
+			index = start;
+		}
+		else if (arrayToSort.read(end) == i)
+		{
+			index = end;
+		}
+
+		return index;
+	}
+
+}
