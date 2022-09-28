9e808f46fc5b10a29420828db4066a18c94500f2
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-09-28 12:54:56 -0600

    better

--- IterativeSearcher/src/IterativeSearcher.java
+++ IterativeSearcher/src/IterativeSearcher.java
@@ -1,40 +1,40 @@
 
 
-	public class IterativeSearcher {
+public class IterativeSearcher {
 
-		public int doBinarySearch(IntegerArray arrayToSort, int i) {
+	public int doBinarySearch(IntegerArray arrayToSort, int i) {
 
-			int index = -1;
-			int start = 0;
-			int end = arrayToSort.length() - 1;
-			int mid = (end - start)/2;
+		int index = -1;
+		int start = 0;
+		int end = arrayToSort.length() - 1;
+		int mid = (end - start)/2;
 
-			while ((end - start) > 1)
+		while ((end - start) > 1)
+		{
+			if (arrayToSort.read(mid) < i)
 			{
-				if (arrayToSort.read(mid) < i)
-				{
-					start = mid;
-					mid = mid + (end - start)/2;
-				}
-				else
-				{
-					end = mid;
-					mid = mid - (end - start)/2;
-				}
+				start = mid;
+				mid = mid + (end - start)/2;
 			}
-
-			if (arrayToSort.read(start) == i)
+			else
 			{
-				index = start;
+				end = mid;
+				mid = mid - (end - start)/2;
 			}
-			else if (arrayToSort.read(end) == i)
-			{
-				index = end;
-			}
-
-			return index;
 		}
 
+		if (arrayToSort.read(start) == i)
+		{
+			index = start;
+		}
+		else if (arrayToSort.read(end) == i)
+		{
+			index = end;
+		}
+
+		return index;
 	}
 
 }
+
+
