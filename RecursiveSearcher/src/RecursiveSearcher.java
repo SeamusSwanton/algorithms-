89858eecaaa18585956b827bf2e539cbfe6d618e
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-11-15 14:54:04 -0700

    done

--- RecursiveSearcher/src/RecursiveSearcher.java
+++ RecursiveSearcher/src/RecursiveSearcher.java
@@ -3,19 +3,63 @@ public class RecursiveSearcher {
 	int low;
 	int high;
 	int mid = 0;
-	
+	int output = -1;
+
 	private int doBinarySearch(IntegerArray arrayOfInts, int target, int low, int high) {
-		mid = (high + low)/2;
-		
-		int midValue = arrayOfInts.read(mid);
-		
-		return target;
-	}
+		if (low <= high) {
+			mid = (high + low)/2;
+
+			int midValue = arrayOfInts.read(mid);
+
+			if(midValue == target) {
+				if (mid == low || mid == high) {
+					output = mid;
+				}
+				if (target == arrayOfInts.read(mid-1)) {
+					high = mid;
+					doBinarySearch(arrayOfInts, target, low, high);
+				}
+				output = mid;
+
+			}
+			else if (midValue < target) {
+				low = mid + 1;
+				doBinarySearch(arrayOfInts, target, low, high);
+			}
+			else if (midValue > target) {
+				high = mid - 1;
+				doBinarySearch(arrayOfInts, target, low, high);
+			}
+			else {
+				output = -1;
+			}
+
+			return output;
+		}
+		else {
+			return -1;
+		}
+			
+		}
+	
 	
 	public int doBinarySearch(IntegerArray arrayOfInts, int target) {
 		low = 0;
 		high = arrayOfInts.length()-1;
 
-		return doBinarySearch(arrayOfInts, target, low, high);
+		if (target == arrayOfInts.read(low)) {
+			return low;
+		}
+		else if (target == arrayOfInts.read(high)) {
+			return high;
+		}
+		else {
+			if (low <= high) {
+				return doBinarySearch(arrayOfInts, target, low, high);
+			}
+			else {
+				return -1;
+			}
+		}
 	}
 }
