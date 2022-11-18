7d9b3dc33ec489cc359b00f482ab3966fe5dacff
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-11-18 12:14:28 -0700

    d

--- RecursiveSorter/src/RecursiveSorter.java
+++ RecursiveSorter/src/RecursiveSorter.java
@@ -1,9 +1,34 @@
 public class RecursiveSorter {
 	
 	public void doQuickSort(IntegerArray clone) {
+		
 	}
 
 	public void doMergeSort(IntegerArray array) {
+		int leftIndex = 0;
+		int rightIndex = array.length() - 1;
+		
+		doMergeSort(array, leftIndex, rightIndex);
+	}
+	
+	public void merge(IntegerArray array, int leftIndex,int mid, int rightIndex) {
+		int[] output = new int[0];
+		
+		int i, j, k;  
+	    int n1 = mid - leftIndex + 1;    
+	    int n2 = rightIndex - mid;  
+	}
+	
+	
+	public void doMergeSort(IntegerArray array, int leftIndex, int rightIndex) {	
+		if (leftIndex < rightIndex){
+			int mid = (leftIndex + rightIndex)/2;
+			
+			doMergeSort(array, leftIndex, mid);			
+			doMergeSort(array, mid, rightIndex);
+			merge(array, leftIndex, mid, rightIndex);
+		}
+		
 	}
 
 }
