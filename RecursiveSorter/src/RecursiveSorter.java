8c508e2abce350cc478ab66aba7d507b67ac3aca
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-11-21 13:42:55 -0700

    dfs

--- RecursiveSorter/src/RecursiveSorter.java
+++ RecursiveSorter/src/RecursiveSorter.java
@@ -11,12 +11,50 @@ public class RecursiveSorter {
 		doMergeSort(array, leftIndex, rightIndex);
 	}
 	
-	public void merge(IntegerArray array, int leftIndex,int mid, int rightIndex) {
-		int[] output = new int[0];
-		
-		int i, j, k;  
-	    int n1 = mid - leftIndex + 1;    
-	    int n2 = rightIndex - mid;  
+	public IntegerArray merge(IntegerArray array, int leftIndex, int mid, int rightIndex) {
+		int[] output = new int[array.length()];
+
+
+		int n1 = mid - leftIndex + 1;    
+		int n2 = rightIndex - mid; 
+
+		int tempArrayLeft[] = new int[n1];
+		int tempArrayRight[] = new int[n2];
+
+
+		for (int i = 0; i <n1; i++) {
+			array.write(tempArrayLeft[i], (leftIndex + i));
+		}
+		for (int i = 0; i <n2; i++) {
+			array.write(tempArrayRight[i], (rightIndex + i));
+		}
+
+		int a = 0, b = 0; 
+		int k = leftIndex;
+		while (a < n1 && b < n2) {
+
+			if (tempArrayLeft[a] <=  tempArrayRight[b]) {
+				array.write(output[k], tempArrayLeft[a]);
+				a++;
+			}
+			else {
+				array.write(output[k], tempArrayLeft[b]);
+				b++;
+			}
+			k++;
+		}
+		while (a < n1) {
+			array.write(output[k], tempArrayLeft[a]);
+            a++;
+            k++;
+		}
+		while (b < n2) {
+			array.write(output[k], tempArrayLeft[b]);
+            b++;
+            k++;
+        }
+
+		return array;
 	}
 	
 	
@@ -25,7 +63,7 @@ public class RecursiveSorter {
 			int mid = (leftIndex + rightIndex)/2;
 			
 			doMergeSort(array, leftIndex, mid);			
-			doMergeSort(array, mid, rightIndex);
+			doMergeSort(array, mid + 1, rightIndex);
 			merge(array, leftIndex, mid, rightIndex);
 		}
 		
